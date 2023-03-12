344a5eab16429306f2ba39faaccd29ff519fcebf
sparkmahadi <mahadi.development@gmail.com>
2023-03-12 21:57:13 +0600

    exploring redux thunk

---
+++ src/pages/Main/Home.js
@@ -0,0 +1,50 @@
+import React, { useEffect, useState } from "react";
+import ProductCard from "../../components/ProductCard";
+import { useDispatch } from 'react-redux';
+import { toggleBrand, toggleStock } from './../../redux/actions/filterAction';
+import { useSelector } from 'react-redux';
+
+const Home = () => {
+  const [products, setProducts] = useState([]);
+  const dispatch = useDispatch();
+  const filters  = useSelector((state) => state.filter.filters);
+  const {brands, stock} = filters;
+
+  useEffect(() => {
+    fetch("http://localhost:5000/products")
+      .then((res) => res.json())
+      .then((data) => setProducts(data.data));
+  }, []);
+
+  const activeClass = "text-white  bg-indigo-500 border-white";
+
+  return (
+    <div className='max-w-7xl gap-14 mx-auto my-10'>
+      <div className='mb-10 flex justify-end gap-5'>
+        <button
+        onClick={()=>dispatch(toggleStock())}
+          className={`border px-3 py-2 rounded-full font-semibold ${stock ? activeClass : null } `}
+        >
+          In Stock
+        </button>
+        <button
+        onClick={()=>dispatch(toggleBrand("amd"))}
+        className={`border px-3 py-2 rounded-full font-semibold ${brands.includes("amd") ? activeClass : null}`}>
+          AMD
+        </button>
+        <button
+        onClick={()=>dispatch(toggleBrand("intel"))}
+         className={`border px-3 py-2 rounded-full font-semibold ${brands.includes("intel") ? activeClass : null}`}>
+          Intel
+        </button>
+      </div>
+      <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-14'>
+        {products.map((product) => (
+          <ProductCard key={product.model} product={product} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Home;
---
+++ src/redux/reducers/filterReducer.js
@@ -0,0 +1,47 @@
+import { TOGGLE_BRAND } from "../actionTypes/actionTypes";
+import { TOGGLE_STOCK } from './../actionTypes/actionTypes';
+
+export const initialState = {
+    filters:{
+        brands: [],
+        stock: false,
+    },
+    keyword: ""
+};
+
+export const filterReducer = (state = initialState, action) =>{
+    switch(action.type){
+        case TOGGLE_BRAND:
+            if(!state.filters.brands.includes(action.payload)){
+                return{
+                    ...state,
+                    filters:{
+                        ...state.filters, brands: [...state.filters.brands, action.payload]
+                    }
+                }
+            } else{
+                return{
+                    ...state,
+                    filters:{
+                        ...state.filters,
+                        brands: state.filters.brands.filter(brand => brand !== action.payload)
+                    }
+                }
+            }
+
+        case TOGGLE_STOCK:
+            return{
+                ...state, 
+                filters: {
+                    ...state.filters,
+                    stock: !state.filters.stock
+                }
+            }
+
+        default:
+        return state;
+    }
+
+
+    console.log(state);
+}
